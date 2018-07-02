5f6b8f314f409afe1cb54c9f5851b4419dd15760
ovmvo <shi_sheng_huo@163.com>
2018-07-02 18:03:06 +0800

    adfd

--- Arya/views.py
+++ Arya/views.py
@@ -2,7 +2,7 @@ import os
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.shortcuts import render, get_object_or_404, reverse
 from Arya import models
-import json
+import json, datetime
 
 @csrf_exempt
 def Edit(request):
@@ -13,7 +13,7 @@ def Edit(request):
         '''
         获取数据库中的markdown文件
         '''
-        md = models.Article.objects.all().values('content_md')
+        md = models.Article.objects.filter(id=1).values('content_md')
         return render(request, 'edit.html', {"md": md[0]["content_md"]})
     if request.method == "POST":
         title = request.POST.get("title")
@@ -23,7 +23,24 @@ def Edit(request):
         h5_txt = request.POST.get("html_txt")
         md_txt = request.POST.get("markdown_txt")
         is_recommend = request.POST.get("is_recommend")
-        models.Article.objects.create(title=title, desc=desc, category=1, tag=1, content_h5=h5_txt, content_md=md_txt, is_recommend=is_recommend, user_id=1)
+        date_publish = datetime.datetime.now()
+        get_tag = models.Tag.objects.get(name=tag)
+        get_user = models.User(username="admin")
+        get_category = models.Category.objects.get(name=category)
+        new_data = models.Article(
+            title=title,
+            desc=desc,
+            content_h5=h5_txt,
+            content_md=md_txt,
+            click_count=20,
+            is_recommend=is_recommend,
+            category=get_category,
+            date_publish=date_publish,
+            tag=get_tag,
+        )
+        new_data.save()
+        new_data.user.add(get_user)
+        new_data.save()
         return render(request, 'edit.html',)
 
 
